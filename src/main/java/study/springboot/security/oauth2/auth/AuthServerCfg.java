05d6ebe72d4a0c339f7dd5ede79d502b74c3f6c0
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-03 13:01:42 +0800

    更新代码

---
+++ src/main/java/study/springboot/security/oauth2/auth/AuthServerCfg.java
@@ -0,0 +1,49 @@
+package study.springboot.security.oauth2.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+
+import java.util.concurrent.TimeUnit;
+
+@EnableAuthorizationServer
+@Configuration
+public class AuthServerCfg extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.withClientDetails(clientDetailsService);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+
+        // 配置TokenServices参数
+        DefaultTokenServices tokenServices = new DefaultTokenServices();
+        tokenServices.setTokenStore(endpoints.getTokenStore());
+        tokenServices.setSupportRefreshToken(false);
+        tokenServices.setClientDetailsService(endpoints.getClientDetailsService());
+        tokenServices.setTokenEnhancer(endpoints.getTokenEnhancer());
+        tokenServices.setAccessTokenValiditySeconds((int) TimeUnit.MINUTES.toSeconds(10)); //分钟
+
+        endpoints.authenticationManager(authenticationManager)
+                .tokenStore(tokenStore())
+                .tokenServices(tokenServices);
+    }
+}
--- src/main/java/study/springboot/security/oauth2/auth/details/UserDetailsServiceImpl.java
+++ src/main/java/study/springboot/security/oauth2/auth/details/UserDetailsServiceImpl.java
@@ -1,38 +1,15 @@
 package study.springboot.security.oauth2.auth.details;
 
-import com.google.common.base.Strings;
-import com.google.common.collect.Lists;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
-@Component("userDetailsService")
+@Component
 public class UserDetailsServiceImpl implements UserDetailsService {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if (Strings.isNullOrEmpty(username)) {
-            throw new UsernameNotFoundException("用户名不能为空");
-        }
-        LOGGER.info("加载用户[{}]的信息", username);
-        if (!"wzj".equalsIgnoreCase(username)) {
-            throw new UsernameNotFoundException("用户不存在");
-        }
-        UserDetails userDetails = new CustomUserDetails("wzj", "{noop}123",null);
-        return userDetails;
-    }
-
-    private List<SimpleGrantedAuthority> getGrantedLt() {
-        List<SimpleGrantedAuthority> grantedLt = Lists.newArrayList();
-        grantedLt.add(new SimpleGrantedAuthority("/test"));
-        return grantedLt;
+        return null;
     }
 }
