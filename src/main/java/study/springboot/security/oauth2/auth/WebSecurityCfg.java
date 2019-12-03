ac8d5df33bfc4835cacd570377665238f6d337c2
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-03 11:27:35 +0800

    更新代码

--- src/main/java/study/springboot/security/oauth2/auth/WebSecurityCfg.java
+++ src/main/java/study/springboot/security/oauth2/auth/WebSecurityCfg.java
@@ -1,4 +1,4 @@
-package study.springboot.security.oauth2.auth.config;
+package study.springboot.security.oauth2.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -7,18 +7,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-import study.springboot.security.oauth2.auth.filter.WatchDogFilter;
 
 @Configuration
 public class WebSecurityCfg extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private WatchDogFilter watchDogFilter;
     @Autowired
     private UserDetailsService userDetailsService;
 
@@ -26,19 +20,12 @@ public class WebSecurityCfg extends WebSecurityConfigurerAdapter {
     private AuthenticationSuccessHandler loginSuccessHandler;
     @Autowired
     private AuthenticationFailureHandler loginFailureHandler;
-    @Autowired
-    private LogoutSuccessHandler logoutSuccessHandler;
-    @Autowired
-    private AccessDeniedHandler accessDeniedHandler;
 
     /**
      * （★）HTTP请求安全
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //（▲）过滤器
-        http.addFilterBefore(watchDogFilter, UsernamePasswordAuthenticationFilter.class)
-        ;
         //（▲）认证
         //Basic登录
 //        http.httpBasic();
@@ -64,31 +51,6 @@ public class WebSecurityCfg extends WebSecurityConfigurerAdapter {
 //                .permitAll() //不需要权限认证
                 .anyRequest()  //任何请求
                 .authenticated() //需要身份认证
-        ;
-        //（▲）
-        http.exceptionHandling()
-                .accessDeniedPage("/403") //无权页面
-        //.accessDeniedHandler(accessDeniedHandler) //无权处理器
-        //.authenticationEntryPoint() //认证入口
-        ;
-        //（▲）注销
-        http.logout()
-                .logoutUrl("/doLogout")  //注销url
-                .logoutSuccessUrl("/login.html?logout_success") //注销成功url
-        //.logoutSuccessHandler(logoutSuccessHandler) //注销成功处理器
-        ;
-        //（▲）会话
-        http.sessionManagement()
-                .invalidSessionUrl("/login.html?session_invalid") //
-//                .invalidSessionStrategy(invalidSessionStrategy)
-                .maximumSessions(1)
-                .maxSessionsPreventsLogin(false) //当达到最大值时，是否保留已经登录的用户
-        //.sessionRegistry()
-//                .expiredUrl("")
-//                .expiredSessionStrategy(expiredSessionStrategy) //当达到最大值时，旧用户被踢出后的操作
-        ;
-        //（▲）其他
-        http.csrf().disable() //关闭跨站请求防护
         ;
     }
 
