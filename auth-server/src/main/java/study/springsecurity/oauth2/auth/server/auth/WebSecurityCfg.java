4cad0e293651a44aab253097ef97ac4248a88d22
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-05 11:14:32 +0800

    更新代码

--- auth-server/src/main/java/study/springsecurity/oauth2/auth/server/auth/WebSecurityCfg.java
+++ auth-server/src/main/java/study/springsecurity/oauth2/auth/server/auth/WebSecurityCfg.java
@@ -10,14 +10,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
 @Order(2)
 public class WebSecurityCfg extends WebSecurityConfigurerAdapter {
 
